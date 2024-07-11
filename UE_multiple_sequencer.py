c81a82f36fcd7418c76464b7f91676f39000f36c
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-07-11 17:00:20 +0800

    Add files via upload

--- UE_multiple_sequencer.py
+++ UE_multiple_sequencer.py
@@ -6,6 +6,15 @@ import os
 (UE default is 30 fps, so we need to get the frame number and convert it!)
 """
 
+# add a new actor into the world
+actor_path = "/Game/MetaHumans/Cooper/BP_Cooper"
+actor_class = unreal.EditorAssetLibrary.load_blueprint_class(actor_path)
+coordinate = unreal.Vector(-25200.0, -25200.0, 100.0)
+editor_subsystem = unreal.EditorActorSubsystem()
+new_actor = editor_subsystem.spawn_actor_from_class(actor_class, coordinate)
+
+
+
 path = "F:\\Jerry\\Vasilisa" #folder that contain all the character folders
 
 for i in os.listdir(path):
@@ -24,7 +33,6 @@ for i in os.listdir(path):
             #create a new level sequence
             asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
 
-            #name the asset_name:
             asset_name = set_path.split("\\")[-1] #get the last part of the path
 
             level_sequence = unreal.AssetTools.create_asset(asset_tools, asset_name, package_path = "/Game/", asset_class = unreal.LevelSequence, factory = unreal.LevelSequenceFactoryNew())
@@ -32,3 +40,29 @@ for i in os.listdir(path):
             level_sequence.set_playback_start(0) #starting frame will always be 0
             
             level_sequence.set_playback_end(value) #end
+
+
+
+
+            #add the actor into the level sequence
+            actor_binding = level_sequence.add_possessable(new_actor)
+
+            transform_track = actor_binding.add_track(unreal.MovieScene3DTransformTrack)
+            anim_track = actor_binding.add_track(unreal.MovieSceneSkeletalAnimationTrack)
+
+		    # Add section to track to be able to manipulate range, parameters, or properties
+            transform_section = transform_track.add_section()
+            anim_section = anim_track.add_section()
+
+		    # Get level sequence start and end frame
+            start_frame = level_sequence.get_playback_start()
+            end_frame = level_sequence.get_playback_end()
+
+		    # Set section range to level sequence start and end frame
+            transform_section.set_range(start_frame, end_frame)
+            anim_section.set_range(start_frame, end_frame)
+
+		    # Refresh to visually see the new tracks and sections added
+            unreal.LevelSequenceEditorBlueprintLibrary.refresh_current_level_sequence()
+
+print("Done!")
