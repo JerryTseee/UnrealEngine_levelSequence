450221d9648937a75660b4fd9ccaee1e572dd544
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-07-16 17:35:38 +0800

    Add files via upload

--- UE_multiple_sequencer.py
+++ UE_multiple_sequencer.py
@@ -1,28 +1,128 @@
 import unreal
 import json
 import os
+import tkinter as tk
+from tkinter import filedialog
 
+
+# function to get the current level sequence and the sequencer objects
+def get_sequencer_objects(level_sequence):
+	world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+	#sequence_asset = unreal.LevelSequenceEditorBlueprintLibrary.get_current_level_sequence()
+	sequence_asset = level_sequence
+	range = sequence_asset.get_playback_range()
+	sequencer_objects_list = []
+	sequencer_names_list = []
+	bound_objects = []
+
+	sequencer_objects_list_temp = unreal.SequencerTools.get_bound_objects(world, sequence_asset, sequence_asset.get_bindings(), range)
+
+	for obj in sequencer_objects_list_temp:
+		bound_objects = obj.bound_objects
+
+		if len(bound_objects)>0:
+			if type(bound_objects[0]) == unreal.Actor:
+				sequencer_objects_list.append(bound_objects[0])
+				sequencer_names_list.append(bound_objects[0].get_actor_label())
+	return sequence_asset, sequencer_objects_list, sequencer_names_list
+
+
+# function to export the face animation keys to a json file
+def mgMetaHuman_face_keys_export(level_sequence):
+	system_lib = unreal.SystemLibrary()
+	root = tk.Tk()
+	root.withdraw()
+
+	face_anim = {}
+
+	world = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_editor_world()
+
+	sequence_asset, sequencer_objects_list,sequencer_names_list = get_sequencer_objects(level_sequence)
+	face_possessable = None
+
+	editor_asset_name = unreal.EditorAssetLibrary.get_path_name_for_loaded_asset(sequence_asset).split('.')[-1]
+	
+	for num in range(0, len(sequencer_names_list)):
+		actor = sequencer_objects_list[num]
+		asset_name = actor.get_actor_label()
+		bp_possessable = sequence_asset.add_possessable(actor)
+		child_possessable_list = bp_possessable.get_child_possessables()
+		character_name = ''
+
+		for current_child in child_possessable_list:
+			if 'Face' in current_child.get_name():
+				face_possessable = current_child
+		
+		if face_possessable:
+			character_name = (face_possessable.get_parent().get_display_name())
+			face_possessable_track_list = face_possessable.get_tracks()
+			face_control_rig_track = face_possessable_track_list[len(face_possessable_track_list)-1]
+			face_control_channel_list = unreal.MovieSceneSectionExtensions.get_all_channels(face_control_rig_track.get_sections()[0])
+			face_control_name_list = []
+
+			for channel in face_control_channel_list:
+				channel_name = str(channel.get_name())
+				channel_string_list = channel_name.split('_')
+				channel_name = channel_name.replace('_' + channel_string_list[-1], '')
+				face_control_name_list.append(channel_name)
+
+			for ctrl_num in range(0, len(face_control_channel_list)):
+				control_name = face_control_name_list[ctrl_num]
+
+				try:
+					numKeys = face_control_channel_list[ctrl_num].get_num_keys()
+					key_list = [None] * numKeys
+					keys = face_control_channel_list[ctrl_num].get_keys()
+					for key in range(0, numKeys):
+						key_value = keys[key].get_value()
+						key_time = keys[key].get_time(time_unit=unreal.SequenceTimeUnit.DISPLAY_RATE).frame_number.value
+						key_list[key]=([key_value, key_time])
+
+					face_anim[control_name] = key_list
+				except:
+					face_anim[control_name] = []
+			
+			character_name = str(character_name)
+			if 'BP_' in character_name:
+				character_name = character_name.replace('BP_', '')
+			if 'BP ' in character_name:
+				character_name = character_name.replace('BP ', '')
+
+			character_name = character_name.lower()
+			print('character_name is ' + character_name)
+			keys_file = filedialog.asksaveasfile(initialfile = str(editor_asset_name) + '_' + character_name + '_face_anim' + '.json', defaultextension=".json",filetypes=[("All Files","*.*"),("JSON file","*.json")])
+
+			keys_file.write('anim_keys_dict = ')
+			keys_file.write(json.dumps(face_anim))
+			print('Face Animation Keys output to: ' + str(keys_file.name))
+		else:
+			print(editor_asset_name)
+			print('is not a level sequence. Skipping.')
+
+
+
+# Below is the main steps:
 """
 (UE default is 30 fps, so we need to get the frame number and convert it!)
 """
 
 # add a new actor into the world
-actor_path = "/Game/MetaHumans/Cooper/BP_Cooper"
+actor_path = "/Game/MetaHumans/Cooper/BP_Cooper" # the path of the actor
 actor_class = unreal.EditorAssetLibrary.load_blueprint_class(actor_path)
-coordinate = unreal.Vector(-25200.0, -25200.0, 100.0)
+coordinate = unreal.Vector(-25200.0, -25200.0, 100.0) # randomly put it on a coordinate of the world
 editor_subsystem = unreal.EditorActorSubsystem()
 new_actor = editor_subsystem.spawn_actor_from_class(actor_class, coordinate)
 
 
 
 animation_sequence = dict()
-#assume the dataset is only 50 folders !!! (important, need to be changed base on real dataset number)
+#assume the dataset is only 50 folders !!! Important, need to be changed base on real dataset number
 for i in range(2,50):
     animation_sequence[i] = False
 
 
 
-path = "F:\\Jerry\\Vasilisa" #folder that contain all the character folders
+path = "F:\\Jerry\\Vasilisa" #folder that contain all the character folders, need to be changed base on the real path
 
 #for every character folder, start to do the work:
 for i in os.listdir(path):
@@ -36,7 +136,7 @@ for i in os.listdir(path):
                 data = json.load(file)
                 
             frames_value = data["frames"]
-            value = frames_value // 2 #this is the upper bound of the frame range
+            value = frames_value // 2 + 1 #this is the upper bound of the frame range
 
             #create a new level sequence
             asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
@@ -51,9 +151,8 @@ for i in os.listdir(path):
 
 
 
-
             #start to load into the animation to the current face track:
-            #assuming that the name of the animation sequence is "AS_va + number"
+            #assuming that the name of the animation sequence is "AS_va + number" ! Need to be changed base on the real name
             face_anim_path = "/Game/MetaHumans/AS_va"
             #then from low to high to load the animation sequence into the current face track (if the sequenced is loaded before, it will not be loaded again, then go the next)
             for i in range(2,50):
@@ -91,8 +190,6 @@ for i in os.listdir(path):
             anim_section.set_range(start_frame, end_frame)
 
 
-
-
         
             #add face animation track
             components = new_actor.get_components_by_class(unreal.SkeletalMeshComponent)
@@ -117,10 +214,29 @@ for i in os.listdir(path):
             transform_section2.set_range(start_frame, end_frame)
             anim_section2.set_range(start_frame, end_frame)
             
+            
+            # bake to control rig to the face
+            print("level sequence: " + str(level_sequence))
+
+            editor_subsystem = unreal.UnrealEditorSubsystem()
+            world = editor_subsystem.get_editor_world()
+            print("world: " + str(world))
+
+            anim_seq_export_options = unreal.AnimSeqExportOption()
+            print("anim_seq_export_options: " + str(anim_seq_export_options))
+
+            control_rig = unreal.load_object(name = '/Game/MetaHumans/Common/Face/Face_ControlBoard_CtrlRig', outer = None)
+            control_rig_class = control_rig.get_control_rig_class()# use class type in the under argument
+            print("control rig class: " + str(control_rig_class))
+            
+            unreal.ControlRigSequencerLibrary.bake_to_control_rig(world, level_sequence, control_rig_class = control_rig_class, export_options = anim_seq_export_options, tolerance = 0.01, reduce_keys = False, binding = face_binding)
 
 
-
-		    # Refresh to visually see the new tracks and sections added
+		    # Refresh to visually see the new level sequence
+            unreal.LevelSequenceEditorBlueprintLibrary.refresh_current_level_sequence()
+			
+            # Export the current face animation keys to a json file
+            mgMetaHuman_face_keys_export(level_sequence)
             unreal.LevelSequenceEditorBlueprintLibrary.refresh_current_level_sequence()
 
-print("Done!")
+print("Well Done! Jerry!")
